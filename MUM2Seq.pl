eafecea8524fd946fc9e737e605cb7ede9f0fc74
Alvarortor <95102990+Alvarortor@users.noreply.github.com>
2023-08-29 14:40:02 -0500

    Update MUM2Seq.pl
    8/29/23 Modified how file reads lines to avoid 0 for header and trailer

--- MUM2Seq.pl
+++ MUM2Seq.pl
@@ -13,7 +13,6 @@ use Bio::SeqIO;
 
 
 #Program START
-
 if ($#ARGV !=2) { #Checks to see if you have all required arguments
 print "Usage: perl MUM2Seq.pl <Yourfile.mums> <ReferenceFile> <QueryFile>\n";
 exit;
@@ -23,22 +22,25 @@ my $Ref_File = $ARGV[1];
 my $Que_File = $ARGV[2];
 
 
+#How many lines are in mums file
+open(MF, '<',$mumsfile) or die "Reference file cannot be opened";
+my $countM = 0;
+$countM++ while <MF>;
+my $Cleancount = $countM-1;
+close MF;
 
 
 my $seqio_obj = Bio::SeqIO->new(-file => $Ref_File, #opening the Reference Fasta File
                              -format => "fasta" );
-while (my $seq_obj = $seqio_obj->next_seq ) {
-	my $seq = $seq_obj->seq;
-no warnings 'numeric';		
-my $count = 0;
+while (my $seq_obj = $seqio_obj->next_seq ){
+	my $seq = $seq_obj->seq;	
 #Get Coords
 open(FH,'<', $mumsfile) or die $!;
 
-
 #Stating it the first time
-no warnings 'numeric';
 no warnings 'uninitialized';
-chomp(my @number = split(' ', <FH>));
+no warnings 'numeric';
+my @number = split(' ', <FH>);
 my $S1 = $number[0]-1;
 my $LEN = $number[2]+1;
 
@@ -46,28 +48,29 @@ my $LEN = $number[2]+1;
 my $substring = substr($seq, $S1, $LEN);
 my $end = $S1 + $LEN ;
 
-#Save the substring
+
+#Save the substring for reference sequences
 my $filename = <"RefSeq.txt">;
-open(my $fh, '>',$filename) or die "Could not open reference output file.";
+open(my $fh,'>',$filename) or die "Could not open reference output file.";
 my $collabels = "Start\tLEN\tEnd\tSequence\n";
 print $fh $collabels;
-print $fh $S1+1,"\t",$LEN,"\t", $end+1,"\t", $substring,"\n";
+#print $fh $S1+1,"\t",$LEN,"\t", $end+1,"\t", $substring,"\n";
 close $fh;
+
+my $line = 0;
 no warnings 'uninitialized';
 #loop until all done
 $filename = <"RefSeq.txt">;
 open($fh, '>>',$filename) or die "Could not update reference file.";
-until( @number < 1)
+until( $line == $Cleancount)
 {
 @number = split(' ', <FH>);
 my $S1 = $number[0]-1;
 $LEN = $number[2];
-
+$line++;
 #Produce substring
-
 my $substring = substr($seq, $S1, $LEN);
 my $end = $S1 + $LEN;
-my $num = ++$count;
 #Write to file
 print $fh $S1+1,"\t",$LEN,"\t", $end+1,"\t", $substring,"\n";
 ;
@@ -75,6 +78,7 @@ print $fh $S1+1,"\t",$LEN,"\t", $end+1,"\t", $substring,"\n";
 close $fh;
 ;
 }
+
 #Finishes process
 close (FH);
 print "Reference Sequences Done!\n";
@@ -86,16 +90,17 @@ $seqio_obj = Bio::SeqIO->new(-file => $Que_File,
 while (my $seq_objQ = $seqio_obj->next_seq ) {
 	my $seqQ = $seq_objQ->seq;
 	
-my $count = 0;
+
 
 #Get Coords
-no warnings 'numeric';
 no warnings 'uninitialized';
 open(FH,'<', $mumsfile) or die $!; #open file
 
 
 #Stating it the first time
-chomp(my @number = split(' ', <FH>)); #Find a way to remove first line here
+no warnings 'numeric';
+my @number = split(' ', <FH>);
+;#Find a way to remove first line here
 my $S2 = $number[1]-1;
 my $LEN = $number[2];
 
@@ -108,24 +113,25 @@ my $filename = <"QueSeq.txt">;
 open(my $fh, '>',$filename) or die "Could not open query output file."; 
 my $collabels = "Start\tLEN\tEND\tSequence\n";
 print $fh $collabels;
-print $fh $S2+1,"\t",$LEN,"\t", $endQ+1,"\t", $substringQ,"\n";
+#print $fh $S2+1,"\t",$LEN,"\t", $endQ+1,"\t", $substringQ,"\n";
 close $fh;
 
+my $line = 0;
 no warnings 'uninitialized';
 #loop until all done
 $filename = <"QueSeq.txt">;
 open($fh, '>>',$filename) or die "Could not update output file.";
-until( @number < 1)
+until( $line == $Cleancount)
 {
 @number = split(' ', <FH>);
 $S2 = $number[1]-1;
 $LEN = $number[2];
+$line++;
 
 #Produce substring
 no warnings 'uninitialized';
 my $substringQ = substr($seqQ, $S2, $LEN);
 my $end = $S2 + $LEN;
-my $num = ++$count;
 #Write to file
 print $fh $S2+1,"\t",$LEN,"\t", $end+1,"\t", $substringQ,"\n";
 ;
@@ -135,6 +141,6 @@ close $fh;
 }
 
 print "Query Sequences Done!\n";
-
+print "Make sure to perform the proofing program afterwards!\n";
 exit 0;
 
